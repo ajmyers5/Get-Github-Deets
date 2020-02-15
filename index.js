7e56de98c3e7b8acf2ff25a310f61ba03c8753d5
Andrew Myers <ajmyers5@gmail.com>
2020-02-15 09:38:50 -0600

    new

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
-const generate = require("./generateHTML");
+const generateHTML = require("./generateHTML");
 
 const questions = [
           {
@@ -21,11 +21,13 @@ function promptUser() {
     return inquirer.prompt(questions)
 }
 
-function writeToFile(fileName, data, res) {
-    // use as data param in the fs.writefile
-    fs.writeFile(fileName, generateHTML(res, data))
+// fs.writeFile doc: fs.writeFile('message.txt', 'Hello Node.js', 'utf8', callback);
+// function writeToFile(fileName, data, res) {
+//     // use as data param in the fs.writefile
+//     fs.writeFile(fileName, generateHTML(res, data))
+
     
-}
+// }
 
 function init() {
     promptUser().then(function(dataReturnedFromPrompt){
@@ -36,12 +38,19 @@ function init() {
 function requestAPI(data){
     console.log(data)
     // create the api url with appropriate information
-    const queryUrl = `https://api.github.com/users/${data.userName}/repos?per_page=100`;
+    const queryUrl = `https://api.github.com/users/${data.userName}`;
 
     axios.get(queryUrl).then(function(res) {
+      console.log(res)
+      console.log(res.data.name)
+      console.log(res.data.location)
+      console.log(res.data.bio)
+      console.log(res.data.public_repos)
+
+      generateHTML(res, color)
         //file name is user name or something
-        let fileName = res.x.y.z + ".html"
-      writeToFile(fileName, res, data)
+      //   let fileName = res.x.y.z + ".html"
+      // writeToFile(fileName, res, data)
       });
 
 }
